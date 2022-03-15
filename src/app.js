faefa6fb03a9a846a6c9fe243e0349b1579f7228
Dans182 <dgaiteiro@gmail.com>
2022-03-15 19:48:01 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronouns = ["our", "my", "your", "their"];
+let adjetives = [
+  "scariest",
+  "happy",
+  "faster",
+  "cool",
+  "great",
+  "big",
+  "small"
+];
+let nouns = [
+  "squirrels",
+  "desk",
+  "guy",
+  "jogger",
+  "racoon",
+  "cat",
+  "car",
+  "motorcycle",
+  "dog"
+];
+let extentions = [".com", ".es", ".net", ".org", ".ve"];
+let dominio = "";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const domainGenerator = (
+  arrayPronombres,
+  arrayAdjetivos,
+  arraySustantivos,
+  arrayExtensiones
+) => {
+  for (let pronombres of arrayPronombres) {
+    for (let adjetivos of arrayAdjetivos) {
+      for (let sustantivos of arraySustantivos) {
+        for (let extensiones of arrayExtensiones) {
+          dominio +=
+            pronombres + adjetivos + sustantivos + extensiones + "<br>";
+        }
+      }
+    }
+  }
+  document.getElementById("generadordeDominios").innerHTML = dominio;
 };
+
+window.onload = domainGenerator(pronouns, adjetives, nouns, extentions);
+
+let boton = document.getElementById("eliminar");
+boton.addEventListener(
+  "click",
+  function() {
+    document.getElementById("generadordeDominios").innerHTML = "";
+  },
+  false
+);
